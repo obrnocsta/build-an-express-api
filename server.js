614d6913be299c239af3f73fec23f1c67fa5d8fc
Bruno Costa <obrnocsta@gmail.com>
2026-03-25 00:40:12 -0300

    feat: cria rota '/api?' e server os dados filtrados com 'req.query' para o cliente

--- server.js
+++ server.js
@@ -4,8 +4,45 @@ import { startups } from "./data/data.js";
 const PORT = 8000;
 const app = express();
 
+let filteredStartups = startups;
+
 app.get("/api", (req, res) => {
-  res.json(startups);
+  const { industry, country, continent, is_seeking_funding, has_mvp } =
+    req.query;
+
+  if (industry) {
+    filteredStartups = filteredStartups.filter(
+      (startup) => startup.industry.toLowerCase() === industry.toLowerCase(),
+    );
+  }
+
+  if (country) {
+    filteredStartups = filteredStartups.filter(
+      (startup) => startup.country.toLowerCase() === country.toLowerCase(),
+    );
+  }
+
+  if (continent) {
+    filteredStartups = filteredStartups.filter(
+      (startup) => startup.continent.toLowerCase() === continent.toLowerCase(),
+    );
+  }
+
+  if (is_seeking_funding) {
+    filteredStartups = filteredStartups.filter(
+      (startup) =>
+        startup.is_seeking_funding ===
+        JSON.parse(is_seeking_funding.toLowerCase()),
+    );
+  }
+
+  if (has_mvp) {
+    filteredStartups = filteredStartups.filter(
+      (startup) => startup.has_mvp === JSON.parse(has_mvp.toLowerCase()),
+    );
+  }
+
+  res.json(filteredStartups);
 });
 
 app.listen(PORT, () => {
