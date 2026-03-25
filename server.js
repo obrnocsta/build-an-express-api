f42d846d67d9c884747a86ec83ce76f054421ae9
Bruno Costa <obrnocsta@gmail.com>
2026-03-25 01:28:32 -0300

    feat: adiciona module 'CORS' para acesso externo por outro protocolo, dominio, e porta

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 import express from "express";
+import cors from "cors";
 import { apiRouter } from "./routes/apiRoutes.js";
 
 const PORT = 8000;
 const app = express();
+
+app.use(cors());
 
 app.use("/api", apiRouter);
 
